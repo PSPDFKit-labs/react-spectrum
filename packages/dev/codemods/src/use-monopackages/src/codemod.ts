1f511d3ce84a176e5f3caa555eb600b0ac099bc7
Ritesh Kumar <rkritesh078@gmail.com>
2025-04-10 13:38:51 +0530

    fixes

--- packages/dev/codemods/src/use-monopackages/src/codemod.ts
+++ packages/dev/codemods/src/use-monopackages/src/codemod.ts
@@ -15,7 +15,7 @@ function areSpecifiersAlphabetized(specifiers: ImportSpecifier[]) {
  *
  * Works for:
  * - `@react-spectrum/*` -> `@adobe/react-spectrum`.
- * - `@react-aria-nutrient/*` -> `react-aria`.
+ * - `@react-aria/*` -> `react-aria`.
  * - `@react-stately/*` -> `react-stately`.
  *
  * By default this will apply to all the above packages, or optionally you can specify which packages to apply this by passing a comma-separated list to the packages option: `--packages=react-aria,react-stately,react-spectrum`.
@@ -31,8 +31,8 @@ export default function transformer(file: FileInfo, api: API, options: Options) 
       monopackage: '@adobe/react-spectrum',
       individualPrefix: '@react-spectrum/'
     },
-    '@react-aria-nutrient/react-aria': {
-      monopackage: '@react-aria-nutrient/react-aria',
+    'react-aria': {
+      monopackage: 'react-aria',
       individualPrefix: '@react-aria-nutrient/'
     },
     'react-stately': {
